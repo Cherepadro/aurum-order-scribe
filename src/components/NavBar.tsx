dec7bfc4ae2f491e0ed8d0feefe6f1f16c73f398
Cherepadro <mrcherepash@gmail.com>
2025-04-30 16:59:48 +0300

    Update NavBar.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,7 +7,7 @@ import { useAuth } from '@/context/AuthContext';
 const NavBar = () => {
   const navigate = useNavigate();
   const location = useLocation();
-  const { clearAuth, employeeName, workshopAddress } = useAuth();
+  const { logout, employeeName, workshopAddress } = useAuth();
 
   const handleLogout = () => {
     logout();
